edef6201bba1069895802d89b01fb6930a37ef01
Xiaomao <fxm@autoforce.net>
2017-03-22 18:17:47 +0800

    second commit

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-function guess() {
-  return "0A0B";
+var res = 1234;
+function guess(arg) {
+  if (arg == 5678) {
+    return {
+      A: 0,
+      B: 0
+    }
+  } else if (arg == 1256) {
+    return {
+      A: 2,
+      B: 0
+    }
+  } else if (arg == res) {
+    return {
+      A: 4,
+      B: 0
+    }
+  }
 }
--- test.main.js
+++ test.main.js
@@ -1,6 +1,30 @@
 describe('GuessGame', function() {
-  it('should return xAxB when call guess', function() {
-    var rv = guess('1234');
-    assert.isOk(rv.match(/\d+A\d+B/));
+  var rv = guess(1234);
+  it('should return Object when call guess', function() {
+    assert.typeOf(rv, 'object');
+  });
+  it('should return Property A when call guess', function() {
+    assert.property(rv, 'A');
+  });
+  it('should return Property B when call guess', function() {
+    assert.property(rv, 'B');
+  });
+});
+describe('insert5678', function() {
+  var rv = guess(5678);
+  it('should return 0A0B when call guess', function() {
+    assert.deepEqual(rv, { A: 0, B: 0 });
+  });
+});
+describe('insert1256', function() {
+  var rv = guess(1256);
+  it('should return 2A0B when call guess', function() {
+    assert.deepEqual(rv, { A: 2, B: 0 });
+  });
+});
+describe('insert1234', function() {
+  var rv = guess(1234);
+  it('should return 4A0B when call guess', function() {
+    assert.deepEqual(rv, { A: 4, B: 0 });
   });
 });
