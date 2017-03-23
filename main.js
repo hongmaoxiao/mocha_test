1f9fbb4b8a2d55f585c39d054e6688ea3b419fed
Xiaomao <fxm@autoforce.net>
2017-03-23 16:28:25 +0800

    test pass

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
-var res = 1234;
+var res = '1234';
 function guess(arg) {
-  if (arg == 5678) {
-    return {
-      A: 0,
-      B: 0
-    }
-  } else if (arg == 1256) {
-    return {
-      A: 2,
-      B: 0
-    }
-  } else if (arg == res) {
-    return {
-      A: 4,
-      B: 0
+  var re = {
+    right: 0,
+    match: 0,
+  }
+  if (arg.length != 4) {
+    return
+  } else {
+    var rightidex = [];
+    for (var i = 0, arglen = arg.length; i < arglen; i++) {
+      for (var j = 0, reslen = res.length; j < reslen; j++) {
+        if (i == j && arg[i] == res[j]) {
+          re.right++;
+          rightidex.push(i);
+        }
+      }
+      if (res.indexOf(arg[i]) > -1 && !isInArray(i, rightidex)) {
+        re.match++;
+      }
     }
   }
+  return {
+    A: re.right,
+    B: re.match
+  }
+}
+
+function isInArray(val, arr) {
+  for (var k = 0, len = arr.length; k < len; k++) {
+    if (arr[k] == val) {
+      return true;
+    }
+  }
+  return false;
 }
